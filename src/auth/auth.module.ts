49df037438aa8aba8fe80f63a57f8cb9872d0377
Simon Oro <simonoro1@outlook.com>
2023-09-01 00:05:44 -0300

    strategy config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,18 +4,22 @@ import { AuthResolver } from './auth.resolver';
 import { UsersService } from 'src/users/users.service';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt/dist';
+import { jwtConstants } from './constants';
+import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
+
 
 @Module({
   imports: [
     UsersModule,
+    PassportModule,
     JwtModule.register({
       global: true,
-      secret: 'hola',
+      secret: jwtConstants.secret,
       signOptions: {expiresIn: '60s'}
     })
   ],
 
-  providers: [AuthResolver, AuthService]
-
+  providers: [AuthResolver, AuthService, LocalStrategy]
 })
 export class AuthModule {}
